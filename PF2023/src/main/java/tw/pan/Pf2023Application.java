07d2dbf5b367eda686e27452a70a71f7b8cb0616
PAN <PAN@DESKTOP-7OOR7VA>
2023-04-17 23:23:07 +0800

    init

---
+++ PF2023/src/main/java/tw/pan/Pf2023Application.java
@@ -0,0 +1,13 @@
+package tw.pan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pf2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pf2023Application.class, args);
+	}
+
+}
